edd1aee8a68fa59b8bc4bccffa3b3c4fbae0827e
johnathananderson <andersonjohnathan@yahoo.com>
2019-01-06 03:58:40 -0500

    Additional changes to landing page

--- unipantry/src/app/app.module.ts
+++ unipantry/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { LandingComponent } from './landing/landing.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LandingCarouselComponent } from './landing/landing-carousel.component';
 import { BrowseAllComponent } from './landing/landing-browse-all.component';
+import { CardCarouselComponent } from './card-carousel/card-carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
     LandingCarouselComponent,
-    BrowseAllComponent
+    BrowseAllComponent,
+    CardCarouselComponent
   ],
   imports: [
     BrowserModule,
--- unipantry/src/app/landing/landing-browse-all.component.ts
+++ unipantry/src/app/landing/landing-browse-all.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class BrowseAllComponent {
-  images = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(() => `https://picsum.photos/200/200?random&t=${Math.random()}`);
+  images = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(() => `https://picsum.photos/200/200?random&t=${Math.random()}`);
 
   constructor() { }
 }
