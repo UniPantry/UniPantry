d847e5393f1debd0b53f284af635534ee8cb0ab3
johnathananderson <andersonjohnathan@yahoo.com>
2019-01-06 20:08:47 -0500

    Adding carousels

--- unipantry/src/app/landing/landing-carousel.component.ts
+++ unipantry/src/app/landing/landing-carousel.component.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
-import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-landing-carousel',
   templateUrl: './landing-carousel.component.html',
-  providers: [NgbCarouselConfig]})
+  styleUrls: ['./landing.component.css']
+})
 export class LandingCarouselComponent {
-  images = [1, 2, 3].map(() => `https://picsum.photos/1600/500?random&t=${Math.random()}`);
+  images = [1, 2, 3].map(() => `https://picsum.photos/1600/680?random&t=${Math.random()}`);
 
-  constructor(config: NgbCarouselConfig) {
-    // customize default values of carousels used by this component tree
-    config.interval = 10000;
-    config.wrap = true;
-    config.keyboard = false;
-    config.pauseOnHover = true;
-  }
+  constructor() {}
 }
