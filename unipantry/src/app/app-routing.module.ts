c99667fd84828d4d8b22bc3f9e9c724dc65a07f2
johnathananderson <andersonjohnathan@yahoo.com>
2019-01-08 00:59:51 -0500

    routing

--- unipantry/src/app/app-routing.module.ts
+++ unipantry/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoggedInLandingComponent } from './logged-in-landing/logged-in-landing.component';
+import { LandingComponent } from './landing/landing.component';
+import { BrowseComponent } from './browse/browse.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'logged-in', component: LoggedInLandingComponent },
+  { path: 'browse', component: BrowseComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
